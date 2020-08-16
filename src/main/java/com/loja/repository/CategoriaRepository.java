22ac948b78e12873020fab6a387c2c51ce2f558e
Marcos Lopes <marcos@dourasoft.com.br>
2020-08-16 12:27:04 -0400

    Configurando conexão segundaria

--- src/main/java/com/loja/repository/CategoriaRepository.java
+++ src/main/java/com/loja/repository/CategoriaRepository.java
@@ -4,6 +4,7 @@ import com.loja.domain.Categoria;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
     
 }
